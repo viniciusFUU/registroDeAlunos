2062aae2d324d1472a1535a5afd15f022c574b9b
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-10-02 13:56:31 -0300

    Alterando list por collection - inicio

--- src/AlunoController.java
+++ src/AlunoController.java
@@ -1,20 +1,16 @@
-public class AlunoController {
-    private Aluno[] listaDeAlunos;
-    int posicaoInserir = 0;
+import java.util.ArrayList;
 
-    public AlunoController(int quantidade){
-        listaDeAlunos = new Aluno[quantidade];
-    }
+public class AlunoController {
+    private ArrayList<Aluno> listaDeAlunos = new ArrayList<>();
+    int posicaoInserir = 0;
     
     public void addAluno(String nome, int matricula, String curso, Prova[] provas){
-        aumentarCapacidadeLista();
-        Aluno aluno = new Aluno(nome, matricula, curso, provas);
-        
+        Aluno aluno = new Aluno(nome, matricula, curso, provas);        
         if (existMatricula(matricula) == false){
-            if(listaDeAlunos[posicaoInserir] == null){
-                listaDeAlunos[posicaoInserir] = aluno;
-                posicaoInserir++;
-            }
+            listaDeAlunos.add(aluno);
+            System.out.println("ALuno adicionado");
+        } else {
+            System.out.println("Aluno já existe");
         }
     }
     
@@ -22,43 +18,21 @@ public class AlunoController {
         String alunoExcluido = "";
         int posicaoExcluida = 0;
         
-        for (int i = 0; i < listaDeAlunos.length-1; i++){
-            if (listaDeAlunos[i].getNome() == nome && listaDeAlunos[i] != null){
+        for (int i = 0; i < listaDeAlunos.size()-1; i++){
+            if (listaDeAlunos.get(i).getNome().equals(nome)){
                 posicaoExcluida = i;
-                alunoExcluido = listaDeAlunos[i].getNome();
-                listaDeAlunos[i] = null;
+                alunoExcluido = listaDeAlunos.get(i).getNome();
+                listaDeAlunos.set(i, null);
                 posicaoInserir--;
                 break;
             }
         }
 
-        if (listaDeAlunos[posicaoExcluida] == null){
-            System.out.println(posicaoInserir);
-            for (int j = posicaoExcluida; j <= posicaoInserir; j++){
-                if (listaDeAlunos[j+1] == null){
-                    listaDeAlunos[j] = null;
-                } else if (listaDeAlunos[j+1] != null){
-                    listaDeAlunos[j] = listaDeAlunos[j+1];
-                }
-            }
-        }
-        
-        return alunoExcluido;
-    }
-
-    public void aumentarCapacidadeLista(){
-        if (listaDeAlunos.length == posicaoInserir+1){
-            Aluno[] novaListAlunos = new Aluno[listaDeAlunos.length * 2];
-
-            for (int i = 0; i < this.listaDeAlunos.length; i++){
-                novaListAlunos[i] = this.listaDeAlunos[i];
-            }
-            this.listaDeAlunos = novaListAlunos;
-        }
+        return "Aluno removido: " + alunoExcluido;
     }
 
     public boolean existMatricula(int matricula){
-        if (listaDeAlunos[0] != null){
+        if (listaDeAlunos.size() != 0){
             for (Aluno aluno : listaDeAlunos){
                 if (aluno != null && aluno.getMatricula() == matricula){
                     System.out.println("Aluno ja existe!");
@@ -67,46 +41,23 @@ public class AlunoController {
                 continue;
             }
         }
-
+        
         return false;
     }
 
     public void updateAluno(String nome, int matricula){
-        for (Aluno aluno : listaDeAlunos){
-            if (aluno != null && aluno.getMatricula() == matricula){
+        for(Aluno aluno : listaDeAlunos){
+            if (aluno.getMatricula() == matricula){
                 aluno.setNome(nome);
             }
         }
     }
 
     public void showAlunos(){
-        Aluno[] listaParaPercorrer = listaDeAlunos;
-
-        for (Aluno aluno : listaParaPercorrer){
+        for (Aluno aluno : listaDeAlunos){
             if(aluno != null){
                 System.out.println(aluno.getNome());
             }
         }
-    }
-
-    public static void main(String[] args) {
-        AlunoController aluno = new AlunoController(2);
-        Prova[] prova1 = {
-            new Prova(8.3),
-            new Prova(7.5),
-            new Prova(9.2),
-            new Prova(7)
-        };
-
-        ProvaController provaController = new ProvaController();
-        System.out.println("Média :" + provaController.calcularMediaPonderada(prova1));
-
-        aluno.addAluno("Vinicius", 1, "banco de dados", prova1);
-        aluno.addAluno("Vinicius", 1, "banco de dados", prova1);
-        aluno.addAluno("Bianca", 2, "banco de dados", prova1);
-        aluno.addAluno("Bianca", 2, "banco de dados", prova1);
-        aluno.showAlunos();
-        aluno.updateAluno("Vinição", 1);
-        aluno.showAlunos();
     }
 }
